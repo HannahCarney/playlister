f622d782a77fe5ee8f0d5f7cfe276b13f1174fd3
matteomanzo <imatteo.manzo@gmail.com>
2015-02-27 13:54:38 +0000

    reset dbs

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
-  db: {
-    production: process.env.MONGO_DEVELOPMENT_URI 
+  newdb: {
+    production: 'mongodb://' + process.env.MONGODB_DEVELOPMENT_USERNAME + ':' + process.env.MONGODB_DEVELOPMENT_PASSWORD + '@' + process.env.MONGODB_DEVELOPMENT_HOST + ':' process.env.MONGODB_DEVELOPMENT_PORT + '/' + process.env.MONGODB_DEVELOPMENT_DB,
+    development: "localhost:27017/playlister"
+
   }
 };
 
