b582b11da227cc55a3ad0f8d5ddf3c5f36836ba6
Andy <andygnewman@gmail.com>
2015-03-02 10:44:33 +0000

    add refreshToken method to add songs

--- models/helpersSpotify.js
+++ models/helpersSpotify.js
@@ -1,14 +1,19 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+var clientId = process.env.SPOTIFY_CLIENT_ID;
+var clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
 exports.addSongsToPlaylist = function(credentials, tracks) {
-  var spotifyApi = new SpotifyWebApi();
-  spotifyApi.setAccessToken(credentials.spotifyAccessToken);
+  var spotifyApi = new SpotifyWebApi( {clientId : clientId,
+                                      clientSecret : clientSecret});
   spotifyApi.setRefreshToken(credentials.spotifyRefreshToken);
-  spotifyApi.addTracksToPlaylist(tracks.spotifyID, tracks.playlistID
-                                , tracks.tracks)
-  .then(function(data) {
-    console.log('Added tracks to playlist!');
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
+  spotifyApi.refreshAccessToken()
+    .then(function(data) {
+      spotifyApi.setAccessToken(data['access_token']);
+      return spotifyApi.addTracksToPlaylist(tracks.spotifyID, tracks.playlistID
+                                      , tracks.tracks)
+    }).then(function(data) {
+      console.log('Added tracks to the playlist!');
+    }).catch(function(err) {
+      console.log('Something went wrong!', err);
+    });
 };
