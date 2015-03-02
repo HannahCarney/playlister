03b02d5fd59519249435b622a12362111eb87ed8
Andy <andygnewman@gmail.com>
2015-03-02 20:12:56 +0000

    404 and 500 pages created but spotify giving state mismatch

--- server.js
+++ server.js
@@ -15,18 +15,17 @@ var monk = require('monk');
 var db = monk(mongoUri);
 
 //router
+var index = require('./routes/index');
 var partyGoer = require('./routes/partyGoer');
 var partyPlanner = require('./routes/partyPlanner');
 var mobileApp = require('./routes/mobileApp');
 
 // Server Set-up
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'))
-    .use(cookieParser());
+
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(expressLayouts);
-
 // Make our db accessible to our router
 app.use(function(req,res,next){
     req.db = db;
@@ -36,13 +35,27 @@ app.use(function(req,res,next){
 app.use('/partygoer', partyGoer);
 app.use('/partyplanner', partyPlanner);
 app.use('/mobileapp', mobileApp);
+app.use('/', index);
+app.use(express.static(__dirname + '/public'))
+    .use(cookieParser());
+// Handle 404
+app.use(function(req, res) {
+    res.status(400);
+   res.render('error/404.ejs', {title: '404: File Not Found'});
+});
+// Handle 500
+app.use(function(error, req, res, next) {
+    res.status(500);
+   res.render('error/500.ejs', {title:'500: Internal Server Error', error: error});
+});
+
 
 app.set('port', (process.env.PORT || 3000));
 
-// // Routes
-app.get('/', function(req, res){
-  res.render('index');
-});
+// Routes
+// app.get('/', function(req, res){
+//   res.render('index');
+// });
 
 
 
