9b64f1a51d437ee1fa051efdf7c1e4e036e03918
Andy <andygnewman@gmail.com>
2015-03-05 16:34:07 +0000

    refactoring

--- public/js/listUpdate.js
+++ public/js/listUpdate.js
@@ -2,36 +2,37 @@ var list=[];
 var maxSongs = 2; //to read from the server initially
 var error;
 
+//Click 'add song' button
 $("#addSong").click(function(){
   firstValidation(selectedSongId);
 });
 
+//Click 'search' button
 $("#search").click(function(){
   $('#addSong').removeAttr('disabled')
   error = "";
   $('#errormessage').text(error);
 });
 
-firstValidation = function(selectedSong){
-   if (selectedSong == "") {
-    error = "You need to select a song";
-    $('#errormessage').text(error);
-   }
-   else {
-    loadSongsToForm(selectedSong);
-   }
-};
-
-
+//Click 'delete from short list' song button
 $('ul').on('click','button',function(el){
-  console.log('This id: ' + this.id);
   song = this.id;
   deleteFromTheList(list,song);
   $(this).parent().remove();
   $('#addSong').removeAttr('disabled');
 });
 
-deleteFromTheList = function(list,song){
+var firstValidation = function(selectedSong){
+   if (selectedSong == "") {
+    error = "You need to select a song";
+    $('#errormessage').text(error);
+   }
+   else {
+    validateSongChoice(selectedSong);
+   }
+};
+
+var deleteFromTheList = function(list,song){
   var index = -1;
   for(var i=0; i < list.length; i++){
     if (list[i].spotifyID == song){
@@ -44,18 +45,18 @@ deleteFromTheList = function(list,song){
   }
 };
 
-loadSongsToForm = function(song) {
-  if (checkIfSongInList(song) === false) {
+var validateSongChoice = function(song) {
+  if (checkIfSongAlreadyInList(song) === false) {
     var ppPartyName = $('#pp-party-name').text();
     var ppPartyDate = $('#pp-party-date').text();
     var singleSongChoice = song;
     serverVerifySong(location.origin,"/verifySong",{ppPartyName: ppPartyName, ppPartyDate: ppPartyDate, singleSongChoice: singleSongChoice},function(json){
       if (json.songChoiceAllowed == false) {
-        error = "Song has already been picked";
+        error = "Song has been picked for this party already";
       $('#errormessage').text(error);
       }
       else {
-        validate(song);
+        loadSongsToList(song);
         var songIDList = [];
         for (var i = 0; i < list.length; i++) {
           songIDList.push(list[i].spotifyID);
@@ -72,31 +73,30 @@ loadSongsToForm = function(song) {
   }
 };
 
-serverVerifySong = function(path,ext,object,callback){
-    $.ajax({
-             type: "GET",
-             dataType: 'json',
-             url: path+ext, //ext = '/qry'
-             data: object,
-             success: function(json){
-                 callback(json);
-             }
-         });
+var serverVerifySong = function(path,ext,object,callback){
+  $.ajax({
+          type: "GET",
+          dataType: 'json',
+          url: path+ext, //ext = '/qry'
+          data: object,
+          success: function(json){
+            callback(json);
+          }
+        });
 };
 
-validate = function(selectedSong) {
-
-    var id = "#"+selectedSong.substring(14)+'1';
-    var name = $(id).attr('idName');
-    list.push({spotifyID:selectedSong,name:name});
-    text = name + '<button class="btn btn-inverse buttonX" id="'+selectedSong+'">x</button>';
-    $('<li />',{html: text}).appendTo('ul.songList');
-    if (list.length === maxSongs) {
-      $('#addSong').attr('disabled', 'disabled');
-    }
+var loadSongsToList = function(selectedSong) {
+  var id = "#"+selectedSong.substring(14)+'1';
+  var name = $(id).attr('idName');
+  list.push({spotifyID:selectedSong,name:name});
+  text = name + '<button class="btn btn-inverse buttonX" id="'+selectedSong+'">x</button>';
+  $('<li />',{html: text}).appendTo('ul.songList');
+  if (list.length === maxSongs) {
+    $('#addSong').attr('disabled', 'disabled');
+  }
 };
 
-var checkIfSongInList = function(song) {
+var checkIfSongAlreadyInList = function(song) {
   var index = -1;
   for(var i=0; i < list.length; i++){
     if (list[i].spotifyID == song){
